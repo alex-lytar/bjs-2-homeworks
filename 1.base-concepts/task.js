1afb849fc3d2f2a965289fdb35c60595ed35d5e8
alex <alex@mail.com>
2022-10-31 20:00:56 +0300

    111

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -14,34 +14,29 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  "use strict";
-  let percentTest = Number(percent);
-  let contributionTest = Number(contribution);
-  let amountTest = Number(amount);
-  
-  if (percentTest != Number(percent)){
-	alert(`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
-    return totalAmount	
-  } else if (contributionTest != Number(contribution)){
-    alert(`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
-    return totalAmount  
-  } else if (amountTest != Number(amount)){
-    alert(`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
-    return totalAmount  
+  "use strict"; 
+  let totalAmount = 0; 
+  let mounthPercent = (percent / 100) / 12;
+  let firstPayment = parseInt(contribution);
+  let objectPrice = parseInt(amount);
+  if (Number.isNaN(mounthPercent)){
+    return totalAmount	= `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (Number.isNaN(firstPayment)){
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;  
+  } else if (Number.isNaN(objectPrice)){
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;  
 	}
 	  else {
-  let totalAmount;
-  let bodyCredit = amount - contribution;
+  let bodyCredit = objectPrice - firstPayment;
   const dateNow = new Date();
   const dateFuture = new Date(date);
-  let dateTerm = Math.floor((dateFuture - dateNow) / 1000 / 60 / 60 / 24 / 30);
-  let mounthPercent = (percent / 100) / 12; 
+  let dateTerm = Math.floor((dateFuture - dateNow) / 1000 / 60 / 60 / 24 / 30); 
   let payDay = bodyCredit * (mounthPercent + (mounthPercent / (((1 + mounthPercent) ** dateTerm) - 1)));
   totalAmount = payDay * dateTerm;
   return Number(totalAmount.toFixed(2));
     }
 
-  // код для задачи №2 писать здесь
+  // код для задачи №2  писать здесь
 
 
 
