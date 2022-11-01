947b04536ebff7f30d34e90255800d247f9a262c
alex <alex@mail.com>
2022-11-01 19:50:17 +0300

    43

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,23 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity; 
+  max = -Infinity;
+  sum = 0;
+  avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
+  return { min:min, max:max, avg:avg };
+
+
 
   // Ваш код
 
@@ -8,8 +25,11 @@ function getArrayParams(arr) {
 }
 
 // Задание 2
-function worker(arr) {
-  let sum;
+let func = function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+ }
 
   // Ваш код
 
@@ -17,15 +37,22 @@ function worker(arr) {
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
+  let maxMass = 0;
 
   // Ваш кода
   // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    maxMass = func(arrOfArr[i]);
+    if (maxMass > max) {
+      max = maxMass;
+    }
+ }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  return Math.abs(getArrayParams(arr).max - getArrayParams(arr).min);
 }
