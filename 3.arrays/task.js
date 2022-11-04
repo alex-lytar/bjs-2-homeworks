c762941c2599f91073b4cccfdf7b73be716ce1a2
alex <alex@mail.com>
2022-11-04 21:09:32 +0300

    asd

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = (arr1, arr2) => arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i])
 
   // Ваш код
 
-  return result; // boolean
+  return result(arr1, arr2); // boolean
 }
-
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(arr3 => arr3 >= 0 ).filter(arr4 => arr4 % 3 === 0).map(arr4 => arr4 * 10 );
 
   // Ваш код
 
