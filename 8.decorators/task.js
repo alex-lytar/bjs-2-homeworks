d9ba116c7eb9bae8ff907aa2c1e73ab2e02d96d1
alex <alex@mail.com>
2022-12-07 23:37:01 +0300

    14

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -4,40 +4,42 @@ function cachingDecoratorNew(func) {
     let hash = rest.join(',');
     let existResult = cache.find(cacheRecord => cacheRecord.hash === hash);
     if (existResult !== undefined) {
-        console.log('Из кэша: ' + existResult.value);
-        return 'Из кэша: ' + existResult.value;
-    } 
-      let value = func.call(this, ...rest);
-      console.log('Вычисляем: ' + value);
-    if (cache.length < 5) {   
-        cache.push({hash, value});
-      } 
-        cache.unshift({hash, value});
-        cache.pop();
-      return 'Вычисляем: ' + value;
+      console.log('Из кэша: ' + existResult.value);
+      return 'Из кэша: ' + existResult.value;
+    }
+    let value = func.call(this, ...rest);
+    console.log('Вычисляем: ' + value);
+    cache.push({ hash, value });
+    if (cache.length > 5) {
+      cache.shift({ hash, value })
+    }
+    return 'Вычисляем: ' + value;
   }
   return wrapper;
-  
-  }
+
+}
 
 
 
-  function debounceDecoratorNew(func, ms) {
-    let count = 0;
-    let allCount = 0;
-    let timeout;
-    let flag = true;
-    return function (...rest) {
-      
-      clearTimeout(timeout);
-        if (!flag) {
-          timeout = setTimeout(() => {
-          func.call(this, ...rest); 
-          flag = true;
-          count++;
-            }, ms);
-        }
-       allCount++;
-      return debounceDecoratorNew.call(this, ...rest);
-    };   
-  }
+function debounceDecoratorNew(func) {
+  // Ваш код
+  let isCooldown = false;
+  let timeout = null;
+
+  const wrapper = function() {
+    wrapper.allCount += 1;
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => (isCooldown = false), this.ms);
+    if (isCooldown) {
+      wrapper.count += 1;
+      return;
+    }
+    func.apply(this, arguments);
+    isCooldown = true;
+  };
+  wrapper.count = 0
+  wrapper.allCount = 0
+  return wrapper
+}
